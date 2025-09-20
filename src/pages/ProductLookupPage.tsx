04132b1035c675035271710f208efc249d0ef303
Shashwat2005 <saxenashashwat58@gmail.com>
2025-09-20 21:13:14 +0530

    Create ProductLookupPage.tsx

---
+++ src/pages/ProductLookupPage.tsx
@@ -0,0 +1,10 @@
+// src/pages/ProductLookupPage.tsx
+import ProductLookup from "../components/ProductLookup";
+
+export default function ProductLookupPage() {
+  return (
+    <div className="p-6">
+      <ProductLookup />
+    </div>
+  );
+}
